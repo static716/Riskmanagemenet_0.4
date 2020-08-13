390eedd471cd158aa0b700128baf23d6afccf457
static716 <41085995+static716@users.noreply.github.com>
2020-08-12 21:40:02 -0700

    My Current Progress 08122020

---
+++ BackendStuff/Database/Database_Tools.py
@@ -0,0 +1,110 @@
+import sqlite3
+from datetime import *
+
+# Gets database table name
+def get_Database_Table_Name():
+    today = str(date.today()).replace("-", "")
+    today = "a" + today
+    return today
+
+# Returns the current time
+def getCurrentTime():
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+
+#Customize Time to your preference
+    # current_time = "01:00:00"
+    return current_time
+
+# Adds item to Trade Log Database
+def add_Table_To_Database(list, db_Table_Name):
+    DB_Table_Name = str(db_Table_Name)
+
+    # list[0] = list[0].replace(".", "")
+    # list[1] = list[1].replace("-", "")
+
+    connection = sqlite3.connect('TradeLogSection.db')  ## create or connect to database
+    myCursor = connection.cursor()
+
+    myCursor.execute(
+        "INSERT INTO " + DB_Table_Name + " VALUES (:Time, :Symbol, :Side, :Price, :Quantity, :Route, :ProfitLoss)",
+        {
+            'Time': str(list[0]),
+            'Symbol': str(list[1]),
+            'Side': str(list[2]),
+            'Price': str(list[3]),
+            'Quantity': str(list[4]),
+            'Route': str(list[5]),
+            'ProfitLoss': str(list[6])
+        })
+
+    connection.commit()
+    connection.close()
+
+def reorganize_DB_Table_In_Ascend_Order(db_Table_Name):
+    DB_Table_Name = str(db_Table_Name)
+    connection = sqlite3.connect('TradeLogSection.db')  ## create or connect to database
+    myCursor = connection.cursor()
+    myCursor.execute("SELECT * from " + DB_Table_Name + " ORDER BY Time DESC")
+    databaseInfoList = myCursor.fetchall()
+
+    myCursor.execute("DELETE FROM " + DB_Table_Name)
+    connection.commit()
+    connection.close()
+
+    for i in range(len(databaseInfoList)):
+        add_Table_To_Database(databaseInfoList[i], DB_Table_Name)
+
+def compare_Time_Difference(databaseTime, startTime, timeList):
+
+    databaseTime = databaseTime.split()
+    startTime = startTime.split()
+    timeList = timeList.split()
+
+    database_Year = str(databaseTime[0][1]) + str(databaseTime[0][2]) + str(databaseTime[0][3]) + str(databaseTime[0][4])
+    database_Month = str(databaseTime[0][5]) + str(databaseTime[0][6])
+    database_Day = str(databaseTime[0][7]) + str(databaseTime[0][8])
+
+    startTimeHour = str(startTime[0][0]) + str(startTime[0][1])
+    startTimeMinute = str(startTime[0][3]) + str(startTime[0][4])
+    startTimeSecond = str(startTime[0][6]) + str(startTime[0][7])
+
+    timeListHour = str(timeList[0][0]) + str(timeList[0][1])
+    timeListMinute = str(timeList[0][3]) + str(timeList[0][4])
+    timeListSecond = str(timeList[0][6]) + str(timeList[0][7])
+
+    startTime1 = datetime(int(database_Year), int(database_Month), int(database_Day), int(startTimeHour), int(startTimeMinute), int(startTimeSecond))
+    timeList1 = datetime(int(database_Year), int(database_Month), int(database_Day), int(timeListHour), int(timeListMinute), int(timeListSecond))
+
+    if timeList1 >= startTime1:
+        return True
+    else:
+        return False
+
+def check_If_Already_Exist(list, startTime):
+    dbName = get_Database_Table_Name()
+    connection = sqlite3.connect('TradeLogSection.db')
+    myCursor = connection.cursor()
+
+    rowCount = myCursor.execute("SELECT count(*) FROM " + dbName + "")
+    rowCount = myCursor.fetchall()
+    rowCount = int(rowCount[0][0])
+
+    rowInfo = myCursor.execute("SELECT * FROM " + dbName + "")
+    rowInfo = myCursor.fetchall()
+
+    for currentRowSelected in range(rowCount):
+        if compare_Time_Difference(dbName, startTime, rowInfo[currentRowSelected][0]):
+            if rowInfo[currentRowSelected][0] == list[0]:
+                count = 0
+                for columnz in range(7):
+                    if rowInfo[currentRowSelected][columnz] == list[columnz]:
+                        count = count + 1
+                        # print(count)
+                        if count == 7:
+                            return True
+
+    connection.commit()
+    connection.close()
+    return False
+
---
+++ BackendStuff/Scanning/Scanning_Tools.py
@@ -0,0 +1,84 @@
+import cv2
+import pytesseract
+import numpy as np
+from PIL import ImageGrab
+from datetime import *
+
+# pytesseract file location
+pytesseract.pytesseract.tesseract_cmd = r"C:\Users\bryan\AppData\Local\Tesseract-OCR\tesseract.exe"
+
+# Returns a Screen Shot of specified Desktop Location
+def desktop_Screenshot(x1, y1, x2, y2, resizeX, resizeY):
+    img = ImageGrab.grab(bbox=(x1,y1,x2,y2))
+    img = np.array(img)
+    img = cv2.resize(img, None, fx=resizeX, fy=resizeY)
+    return img
+
+
+#########################################################################################################
+                                        #Get Contour is needed for findColor function
+
+#Gets the outline from given image
+def getContours(img):
+    contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    x, y, w, h = 0, 0, 0, 0
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if area > 500:
+            peri = cv2.arcLength(cnt, True) #Calculates a contour perimeter or a curve length
+            approx = cv2.approxPolyDP(cnt, 0.02 * peri, True) #Approximates a polygonal curve(s) with the specified precision
+            x, y, w, h = cv2.boundingRect(approx) #Calculates the up-right bounding rectangle of a point set or non-zero pixels of gray-scale image
+    return x + w // 2, y
+
+#Returns the color value from given image if our specified color is found (0 = None, 1 = Red, 2 = Blue).
+def findColor(img, myColors): #We have the image, the colors we r looking for, and colors to paint our image
+    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV) #We convert our image color
+    count = 0
+    newPoints = []
+    for color in myColors:
+        lower = np.array(color[0:3])    #1.
+        upper = np.array(color[3:6])    #2.
+        mask = cv2.inRange(imgHSV, lower, upper) #With 1. and 2., we are able to create a mask that detects our color.
+        x, y = getContours(mask)    #We are detecting our shape and in return getting the position of our shape
+        if x != 0 and y != 0:
+            newPoints.append([x, y, count]) #This will store where a circle had been created on our image
+        count += 1
+    return newPoints #This will update our newpoints array
+#########################################################################################################
+#Returns a list of text scanned at specified image location
+def retrieveText(img, itemsScanning):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    boxes = pytesseract.image_to_data(gray, lang='eng', config='-c tessedit_char_blacklist=()|/\`!@#$%^&*-+{}[];><,?_{,=~')
+    boxes = boxes.splitlines()
+    count = 0
+    list = []
+    for a, b in enumerate(boxes):
+        # b = boxes[a].splitlines()  # This Is The Row # This converts it into rows but the data is stored in one column ########Also by changing the index, we change the row that we are looking at
+        # b = b[0].split()  # This converts the one column into multiple columns
+        if a != 0:
+            b = b.split()
+            if len(b) == 12:
+                if len(list) < itemsScanning:
+                    list.append(b[11])
+                    if len(list) == 4:
+                        if list[3].rfind(".") == 4:
+                            hello = list[3].split()
+                            list[3] = str(hello[0][0]) + str(hello[0][1])+ str(hello[0][2])+ str(hello[0][3])
+
+                    if len(list) == 3:
+                        if list[2].rfind("s") == 1:
+                            hello = list[2].split()
+                            list[2] = str(hello[0][0])
+
+                    if len(list) == 7:
+                        if list[6].rfind(":") == 0:
+                            # hello = list[6].split()
+                            list[6] = list[6].replace(":", "-")
+                            # list[6] = "-" + str(hello[0][1]) + str(hello[0][2])+ str(hello[0][3])+ str(hello[0][4])+ str(hello[0][5])
+
+                    if len(list) == itemsScanning:
+                        return list
+#########################################################################################################
+                                #Useful Functions
+

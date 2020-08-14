352a1fe4243baa6b6470295f38329279c2acd1af
static716 <41085995+static716@users.noreply.github.com>
2020-08-13 22:48:19 -0700

    Updated Import_Tradelog_Data_To_Database

--- BackendStuff/Database/Import_Tradelog_Data_To_Database.py
+++ BackendStuff/Database/Import_Tradelog_Data_To_Database.py
@@ -16,72 +16,88 @@ class initiate:
 
         session = True
         while session:
-            #We scan tradelog and try to identify color
+            # First CHeck If Tradelog section is empty or not
             tradeLog_Color = SCN_Tools.identify_Tradelog_Color(startingCordinates, 6, 6)
-            #If there is previous data, then we try to find out what type of data it is
-
-            if tradeLog_Color != 0:
-                # If it is the color that coordinates with out function, then we perform the add_to_database function
-                if tradeLog_Color == the_Color_IM_Looking_For:
-                    print("Scanning Tradelog Color Detected")
-                    DB_Tools.precheck_Before_Uploading_To_Database(startingCordinates,current_time,the_Color_IM_Looking_For)
+            print(tradeLog_Color)
+            if tradeLog_Color != 0:  # If It isn't empty, perform the following
+                scanTextImg = SCN_Tools.desktop_Screenshot(startingCordinates[0], startingCordinates[1],startingCordinates[2], startingCordinates[3], 3, 3)  ##
+                retrieveTimeList = SCN_Tools.retrieveText(scanTextImg, 1)
+                if DB_Tools.compare_Time_Difference(DB_Tools.get_Database_Table_Name(), current_time,retrieveTimeList[0]):
+                    if tradeLog_Color == the_Color_IM_Looking_For:
+                        retrieveTradeList = SCN_Tools.retrieveText(scanTextImg, 7)  ##
+                        # Check if already exist in database
+                        if DB_Tools.check_If_Already_Exist(retrieveTradeList, current_time):
+                            startingCordinates[1] = startingCordinates[1] + 23
+                            startingCordinates[3] = startingCordinates[3] + 23
+                        else:
+                            DB_Tools.add_Table_To_Database(retrieveTradeList, DB_Tools.get_Database_Table_Name())
+                            startingCordinates[1] = startingCordinates[1] + 23
+                            startingCordinates[3] = startingCordinates[3] + 23
+                    else:
+                        startingCordinates[1] = startingCordinates[1] + 23
+                        startingCordinates[3] = startingCordinates[3] + 23
+                else:
+                    DB_Tools.reorganize_DB_Table_In_Ascend_Order(DB_Tools.get_Database_Table_Name())
                     session = False
                     break
-
-                #If color is not what we are looking for
-                else:
-                    #Then we will look at the pending order section cuz the order might be pending
-
-                    pending_Order_Color = SCN_Tools.identify_Tradelog_Color([448, 782, 640, 798], 6, 6)
-                    if pending_Order_Color == 0 or pending_Order_Color == the_Color_IM_Not_Looking_For: #If the color is not there then we will scan and check trade log
-                        tradeLog_Color = SCN_Tools.identify_Tradelog_Color(startingCordinates, 6, 6)
-                        if tradeLog_Color == 0 or tradeLog_Color == the_Color_IM_Not_Looking_For:
-                            session = False
-                            break
-                        else:
-                            DB_Tools.precheck_Before_Uploading_To_Database(startingCordinates,current_time,the_Color_IM_Looking_For)
-
-                            # addToDatabase(current_time, 1)
-                            session = False
-                            break
-                    else:#If it is, then we will wait until the order is no longer pending
-
-                        if pending_Order_Color == 0:
-                            tradeLog_Color = SCN_Tools.identify_Tradelog_Color(startingCordinates, 6, 6)
-                            if tradeLog_Color == the_Color_IM_Looking_For:
-                                DB_Tools.precheck_Before_Uploading_To_Database(startingCordinates,current_time,the_Color_IM_Looking_For)
-
-                                # addToDatabase(current_time, 1)
-                                session = False
-                                break
-
-                            else:
-                                session = False
-                                break
-            else:#If the tradelog is empty or the color is zero
-                pending_Order_Color = SCN_Tools.identify_Tradelog_Color([448, 782, 640, 798], 6, 6) #We will check the order pending section
-                if pending_Order_Color == 0 or pending_Order_Color == the_Color_IM_Not_Looking_For: #If it is not there we will then check the trade log section
+            #############################################################################################################
+            else:
+                pending_Order_Color = SCN_Tools.identify_Tradelog_Color([448, 782, 640, 798], 6, 6)
+                if pending_Order_Color == 0 or pending_Order_Color == the_Color_IM_Not_Looking_For:
                     tradeLog_Color = SCN_Tools.identify_Tradelog_Color(startingCordinates, 6, 6)
-                    if tradeLog_Color == 0 or tradeLog_Color == the_Color_IM_Not_Looking_For:
+                    if tradeLog_Color == 0 or tradeLog_Color == the_Color_IM_Not_Looking_For:  ####
+                        DB_Tools.reorganize_DB_Table_In_Ascend_Order(DB_Tools.get_Database_Table_Name())
                         session = False
                         break
                     else:
-                        DB_Tools.precheck_Before_Uploading_To_Database(startingCordinates,current_time,the_Color_IM_Looking_For)
-
-                        session = False
-                        break
-                else:#If color does exist in pending order
-
-                    pending_Order_Color = SCN_Tools.identify_Tradelog_Color([448, 782, 640, 798], 6, 6)
-                    if pending_Order_Color == 0: #Then we will wait until it no longer exist and then check trade log again
-                        tradeLog_Color = SCN_Tools.identify_Tradelog_Color(startingCordinates, 6, 6)
-                        if tradeLog_Color == the_Color_IM_Looking_For:
-
-                            DB_Tools.precheck_Before_Uploading_To_Database(startingCordinates,current_time,the_Color_IM_Looking_For)
-
-                            session = False
-                            break
-
+                        scanTextImg = SCN_Tools.desktop_Screenshot(startingCordinates[0], startingCordinates[1],startingCordinates[2], startingCordinates[3], 3, 3)
+                        retrieveTimeList = SCN_Tools.retrieveText(scanTextImg, 1)
+                        if DB_Tools.compare_Time_Difference(DB_Tools.get_Database_Table_Name(), current_time,retrieveTimeList[0]):
+                            if tradeLog_Color == the_Color_IM_Looking_For:
+                                retrieveTradeList = SCN_Tools.retrieveText(scanTextImg, 7)
+                                # Check if already exist in database
+                                if DB_Tools.check_If_Already_Exist(retrieveTradeList, current_time):
+                                    startingCordinates[1] = startingCordinates[1] + 23
+                                    startingCordinates[3] = startingCordinates[3] + 23
+                                else:
+                                    DB_Tools.add_Table_To_Database(retrieveTradeList,DB_Tools.get_Database_Table_Name())
+                                    startingCordinates[1] = startingCordinates[1] + 23
+                                    startingCordinates[3] = startingCordinates[3] + 23
+                            else:
+                                startingCordinates[1] = startingCordinates[1] + 23
+                                startingCordinates[3] = startingCordinates[3] + 23
                         else:
+                            DB_Tools.reorganize_DB_Table_In_Ascend_Order(DB_Tools.get_Database_Table_Name())
                             session = False
                             break
+                #############################################################################################################
+                else:
+                    pending_Order_Color = SCN_Tools.identify_Tradelog_Color([448, 782, 640, 798], 6, 6)
+                    if pending_Order_Color == 0:  # Then we will wait until it no longer exist and then check trade log again
+                        tradeLog_Color = SCN_Tools.identify_Tradelog_Color(startingCordinates, 6, 6)
+                        if tradeLog_Color == 0 or tradeLog_Color == the_Color_IM_Not_Looking_For:  ####
+                            DB_Tools.reorganize_DB_Table_In_Ascend_Order(DB_Tools.get_Database_Table_Name())
+                            session = False
+                            break
+                        else:
+                            scanTextImg = SCN_Tools.desktop_Screenshot(startingCordinates[0], startingCordinates[1],startingCordinates[2], startingCordinates[3], 3,3)
+                            retrieveTimeList = SCN_Tools.retrieveText(scanTextImg, 1)
+                            if DB_Tools.compare_Time_Difference(DB_Tools.get_Database_Table_Name(), current_time,retrieveTimeList[0]):
+                                tradeLog_Color = SCN_Tools.identify_Tradelog_Color(startingCordinates, 6, 6)
+                                if tradeLog_Color == the_Color_IM_Looking_For:
+                                    scanTextImg = SCN_Tools.desktop_Screenshot(startingCordinates[0],startingCordinates[1],startingCordinates[2],startingCordinates[3], 3, 3)
+                                    retrieveTradeList = SCN_Tools.retrieveText(scanTextImg, 7)
+                                    if DB_Tools.check_If_Already_Exist(retrieveTradeList, current_time):
+                                        startingCordinates[1] = startingCordinates[1] + 23
+                                        startingCordinates[3] = startingCordinates[3] + 23
+                                    else:
+                                        DB_Tools.add_Table_To_Database(retrieveTradeList,DB_Tools.get_Database_Table_Name())
+                                        startingCordinates[1] = startingCordinates[1] + 23
+                                        startingCordinates[3] = startingCordinates[3] + 23
+                                else:
+                                    startingCordinates[1] = startingCordinates[1] + 23
+                                    startingCordinates[3] = startingCordinates[3] + 23
+                            else:
+                                DB_Tools.reorganize_DB_Table_In_Ascend_Order(DB_Tools.get_Database_Table_Name())
+                                session = False
+                                break
